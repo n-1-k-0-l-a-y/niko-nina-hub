1cd5603e199051bfaff1a4c571bab8c1b68faf13
Nikolay <h09boi@gmail.com>
2023-06-19 12:19:27 +0300

    remove r animation

--- src/components/HubInfo.tsx
+++ src/components/HubInfo.tsx
@@ -15,15 +15,7 @@ function HubInfo() {
       <HubImage src={hub.data.image} />
       <div>
         <HubLink href={hub.data.externalUrl} target="_blank" rel="noreferrer">
-          <ul className="c-rainbow">
-            <li className="c-rainbow__layer c-rainbow__layer--white">View on Nina</li>
-            <li className="c-rainbow__layer c-rainbow__layer--orange">View on Nina</li>
-            <li className="c-rainbow__layer c-rainbow__layer--red">View on Nina</li>
-            <li className="c-rainbow__layer c-rainbow__layer--violet">View on Nina</li>
-            <li className="c-rainbow__layer c-rainbow__layer--blue">View on Nina</li>
-            <li className="c-rainbow__layer c-rainbow__layer--green">View on Nina</li>
-            <li className="c-rainbow__layer c-rainbow__layer--yellow">View on Nina</li>
-          </ul>
+          View on Nina
         </HubLink>
       </div>
     </HubInfoContainer>
@@ -36,10 +28,9 @@ const HubInfoContainer = styled.div`
 `;
 
 const HubLink = styled.a`
-  text-decoration: none;
   position: fixed;
-  right: 2em;
-  bottom: 0;
+  right: 6px;
+  bottom: 4px;
 `;
 
 const HubImage = styled.img`
